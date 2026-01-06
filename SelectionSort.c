5fe5f85bc2244c4c7b5414784179086d5c32901a
Neel <neelgundale@gmail.com>
2026-01-06 23:53:11 +0530

    Selection Sort Implementation

---
+++ SelectionSort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void selectionSort(int a[], int n) {
+    int i, j, min, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[min])
+                min = j;
+        }
+        // Swap
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+}
+
+int main() {
+    int n, i;
+    int a[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+
+    selectionSort(a, n);
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
