3de60ebabf93bb37b0a740c78c52c80ed2f1a655
Neel <neelgundale@gmail.com>
2026-01-08 00:41:59 +0530

    Added flag

--- FuncBubbleSort.c
+++ FuncBubbleSort.c
@@ -2,18 +2,22 @@
 
 void bubbleSort(int arr[], int n){
 
+  
     for(int i=0; i<n-1; i++){
+        int flag = 0;
         for(int j=0; j<n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = 1;
             }
-        }
-    }
+        }   
+        if(flag == 0){
+            break;
+    }}
 }
-
 int main() {
     int arr[100], n;
 
