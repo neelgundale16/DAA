189f616e25df0b36d91fe634c85131f71c6c0e76
Neel <neelgundale@gmail.com>
2026-01-29 00:14:32 +0530

    Removed unnecessary files

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
+#include <time.h>
 
-int main () {
 
-    int i, j, temp, n;
-    int arr[20];
+void bubbleSort(int arr[], int n){
 
-    printf("Enter number of elements: ");
-    scanf("%d" , &n);
-
-    printf("Enter the elements : ");
-    for (i=0; i<n; i++) {
-        scanf("%d" , &arr[i]);
-    }
-
-    // Bubble Sort Algorithm
-    for (i=0; i<n-1; i++) {
-        for (j=0; j<n-1-i; j++) {
-
-            if (arr[j] > arr[j+1]) {
-                // Swap arr[j] and arr[j+1]
-                temp = arr[j];
+  
+    for(int i=0; i<n-1; i++){
+        int flag = 0;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                
+                int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = 1;
             }
-        }
+        }   
+        if(flag == 0){
+            break;
+    }}
+}
+int main() {
+
+    int arr[100], n;
+
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array : ");
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
     }
 
-    printf("Sorted array is : ");
-    for(i=0; i<n; i++){
-        printf("%d " , arr[i]);
-        
-    }
+    clock_t start = clock();
+
+    bubbleSort(arr, n);
+
+    clock_t end = clock();
+    double timeTaken = (double)(end - start) / CLOCKS_PER_SEC;
+   
+    printf("Sorted array : ");
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }   
+    printf("Time taken by Bubble Sort = %f seconds\n", timeTaken);
+    return 0;
+
 }
