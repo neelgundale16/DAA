782b7776aae440130d4c445d9eabedbd7528a24e
Neel <neelgundale@gmail.com>
2026-02-12 00:41:52 +0530

    Quick Sort Implementation

---
+++ QuickSort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];   // last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            
+            // swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // place pivot at correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+
+    int arr[1000], n;
+
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array : ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array : ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
